12c5982d2e10da217df2fd80aec2a142618b2f08
아이스본 <37140988+IceBorneVR@users.noreply.github.com>
2023-03-18 18:55:43 +0900

    액션바 아이콘 출력 기능 추가
    드개재

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true); // 액션바에 아이콘 표시
+        getSupportActionBar().setIcon(R.drawable.greeting_icon); // 아이콘 이미지 설정
     }
 }
